354de3a2604a092a6904e8be518b5f527caa4050
sa2taka <sa2taka@gmail.com>
2020-02-26 10:48:40 +0900

    :tada: it's

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-center">
+        <logo />
+        <vuetify-logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
+        <v-card-text>
+          <p>
+            Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower
+            developers to create amazing applications.
+          </p>
+          <div class="text-xs-right">
+            <em>
+              <small>&mdash; John Leider</small>
+            </em>
+          </div>
+          <hr class="my-3" />
+          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
+          <br />
+          <a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire">Continue</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import Logo from '@/components/Logo.vue';
+import VuetifyLogo from '@/components/VuetifyLogo.vue';
+
+@Component({
+  components: {
+    Logo,
+    VuetifyLogo,
+  },
+})
+export default class IndexPage extends Vue {}
+</script>
