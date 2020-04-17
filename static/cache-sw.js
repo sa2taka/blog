00eea7009ad287e2e8d9ad919e9602d4115a5c5d
sa2taka <sa2taka@gmail.com>
2020-04-17 23:32:43 +0900

    :wrench: improve sw

--- static/cache-sw.js
+++ static/cache-sw.js
@@ -1,5 +1,7 @@
 workbox.routing.registerRoute(
-  new RegExp('^https://cdn.contentful.com/spaces/xw0ljpdch9v4/environments/master/'),
+  new RegExp(
+    '^https://cdn.contentful.com/spaces/xw0ljpdch9v4/environments/master/'
+  ),
   workbox.strategies.staleWhileRevalidate({
     cacheName: 'entry-cache',
     plugins: [
@@ -15,6 +17,21 @@ workbox.routing.registerRoute(
 
 workbox.routing.registerRoute(
   new RegExp('^https://images.ctfassets.net/xw0ljpdch9v4/.*.(png|jpg|webp)$'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 14, // for 2 weeks
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('^https://blog.sa2taka.com/$'),
   workbox.strategies.cacheFirst({
     cacheName: 'image-cache',
     plugins: [
