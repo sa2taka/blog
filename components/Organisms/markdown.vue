a36d50e6283173b0302777d7d90c49076c054ecc
sa2taka <sa2taka@gmail.com>
2020-04-18 12:43:44 +0900

    :recycle: Divide display markdown file #3

---
+++ components/Organisms/markdown.vue
@@ -0,0 +1,112 @@
+<template>
+  <div v-html="compiledMarkdownText" />
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from 'nuxt-property-decorator';
+import marked from 'marked';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/atom-one-dark.css';
+
+@Component
+export default class PostSlug extends Vue {
+  @Prop({ required: true })
+  markdown!: string;
+
+  created() {
+    setMarkedOptions();
+  }
+
+  get compiledMarkdownText() {
+    return marked(this.markdown);
+  }
+}
+
+const setMarkedOptions = () => {
+  const renderer = new marked.Renderer();
+  renderer.code = (code, _lang) => {
+    let lang = '';
+    let filename = '';
+    if (_lang) {
+      [lang, filename] = _lang.split(':');
+    } else {
+      lang = 'plaintext';
+    }
+    const value = hljs.highlightAuto(code, [lang]).value;
+    let fileElement = '';
+    if (filename !== '') {
+      fileElement = `<div class="filename">${filename}</div>`;
+    }
+    return `${fileElement}<code class="hljs ${lang} ${filename !== '' &&
+      'padding-for-filename'}">${value}</code>`;
+  };
+
+  renderer.codespan = code => {
+    const value = hljs.highlightAuto(code, ['plaintext']).value;
+    return `<code class="hljsspan">${value}</code>`;
+  };
+  marked.setOptions({
+    // code要素にdefaultで付くlangage-を削除
+    langPrefix: '',
+    renderer,
+  });
+};
+</script>
+
+<style>
+.padding-for-filename {
+  padding-top: 2.6em !important;
+}
+
+.filename {
+  color: #eee;
+  display: inline-block;
+  position: absolute;
+  background-color: #777;
+  padding: 2px 4px;
+  word-break: break-all;
+  border-radius: 0 0 5px 0;
+}
+
+blockquote {
+  margin: 0;
+  padding: 1.1em 1em;
+  margin: 2em auto;
+  border-left: 3px solid rgba(128, 128, 128, 0.8);
+  color: #777;
+}
+
+/* 引用元名 */
+blockquote p.author {
+  text-align: right;
+}
+
+blockquote p.author::before {
+  content: '―― ';
+}
+
+/* HACK */
+.v-application code.hljs {
+  display: block;
+  overflow-x: auto;
+  padding: 0.5em;
+  color: #abb2bf;
+  background: #282c34;
+  margin: 0.5em 0;
+}
+
+blockquote p {
+  margin-bottom: 0px !important;
+}
+
+.v-application code.hljsspan {
+  color: #abb2bf;
+  background: #282c34;
+  padding: 0.1em 0.4em;
+}
+
+.v-application code::before,
+.v-application code::after {
+  content: none;
+}
+</style>
--- pages/post/_slug.vue
+++ pages/post/_slug.vue
@@ -26,7 +26,7 @@
           <p class="post-author">by {{ post.fields.author.fields.name }}</p>
         </div>
       </div>
-      <div class="mt-8" v-html="compiledMarkdownText" />
+      <markdown class="mt-8" :markdown="post.fields.body" />
     </article>
   </div>
 </template>
@@ -34,14 +34,17 @@
 <script lang="ts">
 import { Context } from '@nuxt/types';
 import { Vue, Component } from 'nuxt-property-decorator';
-import marked from 'marked';
-import hljs from 'highlight.js';
-import 'highlight.js/styles/atom-one-dark.css';
 
 import { fetchPost } from '../../libs/contentful';
 import { Post } from '../../types/entry';
 
-@Component
+import Markdown from '@/components/Organisms/markdown.vue';
+
+@Component({
+  components: {
+    Markdown,
+  },
+})
 export default class PostSlug extends Vue {
   post!: Post;
 
@@ -51,14 +54,6 @@ export default class PostSlug extends Vue {
     return {
       post,
     };
-  }
-
-  created() {
-    setMarkedOptions();
-  }
-
-  get compiledMarkdownText() {
-    return marked(this.post.fields.body);
   }
 
   get postDate() {
@@ -85,36 +80,6 @@ export default class PostSlug extends Vue {
     };
   }
 }
-
-const setMarkedOptions = () => {
-  const renderer = new marked.Renderer();
-  renderer.code = (code, _lang) => {
-    let lang = '';
-    let filename = '';
-    if (_lang) {
-      [lang, filename] = _lang.split(':');
-    } else {
-      lang = 'plaintext';
-    }
-    const value = hljs.highlightAuto(code, [lang]).value;
-    let fileElement = '';
-    if (filename !== '') {
-      fileElement = `<div class="filename">${filename}</div>`;
-    }
-    return `${fileElement}<code class="hljs ${lang} ${filename !== '' &&
-      'padding-for-filename'}">${value}</code>`;
-  };
-
-  renderer.codespan = code => {
-    const value = hljs.highlightAuto(code, ['plaintext']).value;
-    return `<code class="hljsspan">${value}</code>`;
-  };
-  marked.setOptions({
-    // code要素にdefaultで付くlangage-を削除
-    langPrefix: '',
-    renderer,
-  });
-};
 
 const formatDate = (date: Date) => {
   const fillBy0 = (num: number, length: number) => {
@@ -203,61 +168,5 @@ const formatDate = (date: Date) => {
   background-size: cover;
   display: flex;
   border-radius: 12px;
-}
-
-.padding-for-filename {
-  padding-top: 2.6em !important;
-}
-
-.filename {
-  color: #eee;
-  display: inline-block;
-  position: absolute;
-  background-color: #777;
-  padding: 2px 4px;
-  word-break: break-all;
-  border-radius: 0 0 5px 0;
-}
-
-blockquote {
-  margin: 0;
-  padding: 1.1em 1em;
-  margin: 2em auto;
-  border-left: 3px solid rgba(128, 128, 128, 0.8);
-  color: #777;
-}
-
-/* 引用元名 */
-blockquote p.author {
-  text-align: right;
-}
-
-blockquote p.author::before {
-  content: '―― ';
-}
-
-/* HACK */
-.v-application code.hljs {
-  display: block;
-  overflow-x: auto;
-  padding: 0.5em;
-  color: #abb2bf;
-  background: #282c34;
-  margin: 0.5em 0;
-}
-
-blockquote p {
-  margin-bottom: 0px !important;
-}
-
-.v-application code.hljsspan {
-  color: #abb2bf;
-  background: #282c34;
-  padding: 0.1em 0.4em;
-}
-
-.v-application code::before,
-.v-application code::after {
-  content: none;
 }
 </style>
