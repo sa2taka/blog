3bec384dad60d0cd2a7498885ac3f447ab4ccdff
sa2taka <sa2taka@gmail.com>
2020-04-19 13:39:36 +0900

    :wrench: Add jest

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,18 @@
 module.exports = {
+  transform: {
+    '^.+\\.js$': 'ts-jest',
+    '.*\\.(vue)$': 'ts-jest',
+  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js',
   },
-  moduleFileExtensions: ['js', 'vue', 'json'],
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest',
-  },
+  moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue',
+    '<rootDir>components/**/*.vue',
+    '<rootDir>pages/**/*.vue',
+    '<rootDir>libs/**/*.ts',
   ],
+  transformIgnorePatterns: ['/node_modules/(?!@babel/runtime-corejs2)'],
 };
