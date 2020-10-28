faae79063b7529860d0f76b62a164c99560bc998
sa2taka <sa2taka@gmail.com>
2020-10-29 03:01:08 +0900

    :up: imporve cache strategy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -313,7 +313,7 @@ export default {
     runtimeCaching: [
       {
         urlPattern: '^https://blog.sa2taka.com/$',
-        handler: 'networkFirst',
+        handler: 'staleWhileRevalidate',
         method: 'GET',
       },
       {
@@ -324,7 +324,7 @@ export default {
       },
       {
         urlPattern: '^https://blog.sa2taka.com/post/.*',
-        handler: 'networkFirst',
+        handler: 'staleWhileRevalidate',
         method: 'GET',
       },
       {
