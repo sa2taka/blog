6081e7c6b342d4197ec2a11644ba018cc84d413c
sa2taka <sa2taka@gmail.com>
2020-10-31 00:26:14 +0900

    :wastebasket: delete old google analytics

--- plugins/ga.js
+++ plugins/ga.js
@@ -1,39 +1,13 @@
 /* eslint-disable */
 
 export default ({ app }) => {
-  /*
-   ** クライアントサイドかつプロダクションモードでのみ実行
-   */
   if (process.env.NODE_ENV !== 'production') return;
-  /*
-   ** Google アナリティクスのスクリプトをインクルード
-   */
-  (function(i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    (i[r] =
-      i[r] ||
-      function() {
-        (i[r].q = i[r].q || []).push(arguments);
-      }),
-      (i[r].l = 1 * new Date());
-    (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m);
-  })(
-    window,
-    document,
-    'script',
-    'https://www.google-analytics.com/analytics.js',
-    'ga'
-  );
-
 
   (function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r;
     (i[r] =
       i[r] ||
-      function() {
+      function () {
         (i[r].q = i[r].q || []).push(arguments);
       }),
       (i[r].l = 1 * new Date());
@@ -46,25 +20,17 @@ export default ({ app }) => {
     document,
     'script',
     'https://www.googletagmanager.com/gtag/js?id=G-3VKK45X498',
-    'ga2'
+    'ga'
   );
 
-
-  /*
-   ** 現在のページをセット
-   */
-  ga('create', 'UA-152417689-1', 'auto');
+  ga('js', new Date());
+  ga('config', 'G-3VKK45X498');
   /*
    ** ルートが変更されるたびに毎回実行（初期化も実行される）
    */
-  ga2('js', new Date());
-  ga2('config', 'G-3VKK45X498');
   app.router.afterEach((to, from) => {
-    /*
-     ** Google アナリティクスにページビューが追加されたことを伝える
-     */
-    ga('set', 'page', to.fullPath);
-    ga('send', 'pageview');
-    ga2('config', 'G-3VKK45X498', {'page_path': to.path});
+    ga('config', 'G-3VKK45X498', {
+      page_path: to.path,
+    });
   });
 };
