68397271d09c52e29b48877be957e66170a92fd1
sa2taka <sa2taka@gmail.com>
2021-04-01 19:41:58 +0900

    :up: run 'yarn upgrade --latest` and fix to run

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-import TerserPlugin from 'terser-webpack-plugin';
 import { BASE_URL } from './libs/const';
 
 const nodeExternals = require('webpack-node-externals');
@@ -226,12 +225,6 @@ export default {
     },
     optimization: {
       minimize: true,
-      minimizer: [
-        new TerserPlugin({
-          cache: true,
-          parallel: false,
-        }),
-      ],
     },
 
     extend(config: any, _ctx: any) {
@@ -350,7 +343,6 @@ export default {
   icon: false,
   typescript: {
     ignoreNotFoundWarnings: true,
-    typeCheck: { memoryLimit: 4096 },
   },
   googleAnalytics: {
     id: 'UA-152417689-1',
