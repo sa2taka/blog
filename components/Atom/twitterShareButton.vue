299c412242d7b10b8f5f44e5c0022b5aebe2dd42
sa2taka <sa2taka@gmail.com>
2021-04-10 18:16:28 +0900

    :wheelchair: add aria-label in share-buttons

--- components/Atom/twitterShareButton.vue
+++ components/Atom/twitterShareButton.vue
@@ -7,6 +7,7 @@
     :min-width="64"
     target="_blank"
     rel="noopener noreferrer"
+    aria-label="ツイートを行う"
   >
     <v-icon color="#1DA1F2" v-html="$vuetify.icons.values.twitter" />
     <span v-if="!isSmartphoneWidth" class="ml-2 black--text">ツイートする</span>
